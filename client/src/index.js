810fcf319ca826c3022a77aa74d7f08de28fe998
14harish <harishpc2003@gmail.com>
2023-04-11 18:41:14 +0530

    xcvx

--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore,combineReducers,applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import chatBotReducer from './store/reducer/chatBotReducer';
 import thunk from 'redux-thunk'
 
-const rootReducer = combineReducers({
-  chatBot: chatBotReducer,
+const rootReducer=combineReducers({
+  chatBot:chatBotReducer,
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer,applyMiddleware(thunk));
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <App />
     </Provider>
   </React.StrictMode>
 );
