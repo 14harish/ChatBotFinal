69edaf4c639ed210246721edfd13089d4029bc54
14harish <harishpc2003@gmail.com>
2023-05-08 19:14:21 +0530

    new

--- server/server1.js
+++ server/server1.js
@@ -13,26 +13,40 @@ const manage = new NlpManager({ languages: ['en'], forceNER: true });
 
 
 //questions-------------------------------------------------------------------------------------
-manage.addDocument('en','hello','Next.greet');
-manage.addDocument('en','hi','Next.greet');
-manage.addDocument('en','heii','Next.greet');
-manage.addDocument('en','Thank you for Information','Next.Thank');
-manage.addDocument('en','Bye see you later byee','Next.Thank');
+manage.addDocument('en','hello hi heii','Next.greet'); //greeting
+manage.addDocument('en','Thank you see you later bye byee','Next.Thank'); //greeting
+manage.addDocument('en','fees structure cse fee','Next.Fees'); //cse fees
+manage.addDocument('en','placed student','Next.placementStd'); //placement student
+manage.addDocument('en','company visited count','Next.vistedCom'); //company visited count
+manage.addDocument('en','highest package salary','Next.package'); //highest salary
+manage.addDocument('en','company visited','Next.dout'); //simple company visited
+manage.addDocument('en','top company visited list','Next.company');//top company visited
+manage.addDocument('en','most no number student placed student highest placed year','Next.highYear');
+manage.addDocument('en','average package salary','Next.AveragePackage'); //average salary
+manage.addDocument('en','college cutoff ','Next.cutoff'); //cutoff required
+manage.addDocument('en','Document needed joining','Next.detailCol'); //document neededs
+manage.addDocument('en','working days','Next.workingDay'); //working days
+manage.addDocument('en','college name','Next.clname'); //college name
+manage.addDocument('en','phone allowed','Next.phonecl');//phone detail
+manage.addDocument('en','dress code','Next.dress'); // dress code
+manage.addDocument('en','hostel fees fee','Next.hostelfees'); //hostel fees
+manage.addDocument('en','fees fee','Next.fee'); //verum fee
+manage.addDocument('en','bus fees','Next.Busfees');//bus fees
+manage.addDocument('en','counselling  number  code counsiling','Next.counsilingNo'); //counsiling name
+manage.addDocument('en','sports quota','Next.sportQuota');//sports quota
+manage.addDocument('en','College timing duration','Next.CollegeTime');//college timing
+manage.addDocument('en','first graduation','Next.firstGraduation');//first graduation
+manage.addDocument('en','period timing class timing class duration','Next.PeridDur'); //period duration
+manage.addDocument('en','lab labaratory facility','Next.labFac'); //lab faclities
+manage.addDocument('en','System available','Next.labsystem');//lab system
+manage.addDocument('en','class room classroom provided available','Next.classFac');//class room available
+manage.addDocument('en','HOD name','Next.HOD');//hod name
+manage.addDocument('en','Semester available count sem','Next.sem');//semester available
+manage.addDocument('en','semester fees sem','Next.semfees');//sem fees
+manage.addDocument('en','chatbot creater owner','Next.creater');//creater or owner
+manage.addDocument('en','staff detail proffesor detail','Next.staffdet');// staff detail
+manage.addDocument('en','staff available count proffesor','Next.staff'); // staff count
 
-//cse fees detail
-// manage.addDocument('en','About Cse department fees Struture','Next.Fees');
-// manage.addDocument('en','What is Cse fees Struture Department','Next.Fees');
-// manage.addDocument('en','Tell about cse fees Structure','Next.Fees');
-// manage.addDocument('en','Tell about cse fees details fess fee detail','Next.Fees');
-// manage.addDocument('en','Fees fee','Next.Fees');
-// manage.addDocument('en','what is the year fees','Next.Fees');
-manage.addDocument('en','fees structure fee','Next.Fees');
-
-
-//placement related questions
-manage.addDocument('en','how many placed students','Next.placementStd');
-manage.addDocument('en','placement history','Next.placementStd');
-manage.addDocument('en','history of placement','Next.placementStd');
 
 //Placement Batch detail
 manage.addDocument('en','2023 Batch','Next.23');
@@ -46,129 +60,16 @@ manage.addDocument('en','2016 Batch','Next.16');
 manage.addDocument('en','2015 Batch','Next.15');
 manage.addDocument('en','2014 Batch','Next.14');
 manage.addDocument('en','2013 Batch','Next.13');
-
-//company
-manage.addDocument('en','how many company as visted','Next.vistedCom');
-manage.addDocument('en','how much company visited the college','Next.vistedCom');
-
-manage.addDocument('en','what was the highest package','Next.package');
-manage.addDocument('en','which is highest package to package','Next.package');
-manage.addDocument('en','what is highest salary','Next.package');
-
-// manage.addDocument('en','How many student placed in the highest package','Next.package');
-
-
-manage.addDocument('en','what are top company visited company','Next.company');
-manage.addDocument('en','what are the company is visited','Next.company');
-manage.addDocument('en','say top company visited','Next.company');
-
-manage.addDocument('en','Which year most No of student placed','Next.highYear');
-manage.addDocument('en','In which year most of the student placed and what is the count','Next.highYear');
-
-manage.addDocument('en','what was the average salary','Next.AveragePackage');
-manage.addDocument('en','Average package for the placed student','Next.AveragePackage');
-//Year Fees 
+// //Year Fees 
 manage.addDocument('en','first year I year 1st year 1 st year fees','Next.first');
-// manage.addDocument('en','1st year','Next.first');
 manage.addDocument('en','second year II year 2nd year 2 nd year fees','Next.second');
-// manage.addDocument('en','2nd year','Next.second');
 manage.addDocument('en','third year III year 3rd year 3 rd year fees','Next.third');
-// manage.addDocument('en','3rd year','Next.third');
 manage.addDocument('en','fourth year IV year 4th year 4 th year fees','Next.fourth');
-// manage.addDocument('en','4th year','Next.fourth');
-
-
-//common queries
-manage.addDocument('en','college minimum required cutoff','Next.cutoff');
-manage.addDocument('en','what is college cutoff required','Next.cutoff');
-
-manage.addDocument('en','what are the need of details for joining college','Next.detailCol');
-manage.addDocument('en','Detail needs for college joining','Next.detailCol');
-manage.addDocument('en','What are document needed','Next.detailCol');
-
-manage.addDocument('en','Total no of working days in college','Next.workingDay');
-manage.addDocument('en','how many workig days in the week','Next.workingDay');
-
-manage.addDocument('en','what is college name','Next.clname');
-manage.addDocument('en','Name of the college','Next.clname');
-
-manage.addDocument('en','Is it phone is allowed to college','Next.phonecl');
-manage.addDocument('en','mobile is allowed in the college','Next.phonecl');
-
-//dress code 
-manage.addDocument('en','What is dress code','Next.dress');
-manage.addDocument('en','dress code of the college','Next.dress');
-
-//hostel fees
-manage.addDocument('en','what about hostel fees','Next.hostelfees');
-manage.addDocument('en','hostel fees details','Next.hostelfees');
-
-//Buss fees
-manage.addDocument('en','there any bus fees','Next.Busfees');
-manage.addDocument('en','what about bus fees','Next.Busfees');
-
-//counsiling number
-manage.addDocument('en','what is counselling number','Next.counsilingNo');
-manage.addDocument('en','what is counselling code','Next.counsilingNo');
-manage.addDocument('en','college counselling Number','Next.counsilingNo');
-manage.addDocument('en','college code of counselling ','Next.counsilingNo');
-
-//sports quota
-manage.addDocument('en','There is sports quota','Next.sportQuota');
-manage.addDocument('en','what about sports quota','Next.sportQuota');
-
-//firstGradutaion
-manage.addDocument('en','First graduation detail','Next.firstGraduation');
-manage.addDocument('en','About First Graduation fees ','Next.firstGraduation');
-
-//college timing
-manage.addDocument('en','College Timeing','Next.CollegeTime');
-manage.addDocument('en','What about college duration','Next.CollegeTime');
-
-//period duration
-manage.addDocument('en','period duration','Next.PeridDur');
-manage.addDocument('en','period timing class timing class duration','Next.PeridDur');
-manage.addDocument('en','how many periods are in the one day available','Next.PeridDur');
-manage.addDocument('en','In one day how many periods','Next.PeriodDur');
-
-//lab facility
-manage.addDocument('en','What about lab facility','Next.labFac');
-manage.addDocument('en','Lab facilities laboratory facility','Next.labFac');
-manage.addDocument('en','How many system available in the lab','Next.labsystem');
-manage.addDocument('en','System availability of the laboratory lab','Next.labsystem');
-
-//classroom
-manage.addDocument('en','How many classroom are provided','Next.classFac');
-
-//hod sir
-manage.addDocument('en','HOD name','Next.HOD');
-manage.addDocument('en','name of head of the department','Next.HOD');
-manage.addDocument('en','Name of hod sir','Next.HOD');
-
-//semester
-manage.addDocument('en','How many sem semester available sem','Next.sem');
-manage.addDocument('en','totally how many semester sem','Next.sem');
-manage.addDocument('en','what is semester fees','Next.semfees');
-manage.addDocument('en','How much is semester fees','Next.semfees');
-
-manage.addDocument('en','who created this chatbot website','Next.creater');
-
-//staff
-manage.addDocument('en','how many staff are in the college avialble','Next.staff');
-manage.addDocument('en','staff available in the department how many staff available','Next.staff');
-manage.addDocument('en','is the staff are trained','Next.staffdet');
-manage.addDocument('en','What about staff','Next.staffdet');
-manage.addDocument('en','What about proffsessors','Next.staffdet');
-
-manage.addDocument('en','Tell about cse','Next.cseDept');
-manage.addDocument('en','About cse deparment','Next.cseDept');
-manage.addDocument('en','tell or about Computer science and Engineering','Next.cseDept');
-
 
 //Answers---------------------------------------------------------------------------------
-// manage.addAnswer('en','Next.staff',"Hod name:Dr.A.Rajiv Kannan"); 
 
 manage.addAnswer('en','Next.staff',"There is 29 staff available https://ksrce.ac.in/index.php/page?id=135&item=113"); 
+manage.addAnswer('en','Next.fee',"mention which fees"); 
 manage.addAnswer('en','Next.staffdet',"Staff are company trained and certified staffs"); 
 manage.addAnswer('en','Next.labsystem',"There is two Lab for CSE and there is more than 100+ systems available"); 
 manage.addAnswer('en','Next.creater',"*Harish V *Barath S *Hariharan M"); 
@@ -186,6 +87,7 @@ manage.addAnswer('en','Next.Thank',"Pleasure Any Other Queries"); //greet
 
 //Placement Batch
 manage.addAnswer('en','Next.placementStd',"Mention the Batch or the year");
+manage.addAnswer('en','Next.dout',"count or list");
 manage.addAnswer('en','Next.vistedCom',"50+ company as visited");
 manage.addAnswer('en','Next.package',"7.2 LPA");
 manage.addAnswer('en','Next.company',"*ZOHO *Coding Mart *TCS *virtusa etc....");
