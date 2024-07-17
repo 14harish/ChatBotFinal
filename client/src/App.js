825696977ab8c178ae41f927d4e12f98e1431603
14harish <harishpc2003@gmail.com>
2024-07-17 12:20:50 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -3,43 +3,15 @@ import React, { useState } from "react";
 import Navbar from "./component/Navbar";
 import Bot from "./component/bot";
 
-import ParticlesBackground from "./component/ParticlesBackground";
+// import ParticlesBackground from "./component/ParticlesBackground";
 
 
 function App() {
-  const [direct, SerDirect] = useState(false);
+
 
   return (
-    <div className="bg-custom-image bg-cover bg-center min-h-screen">
-            {/* <ParticlesBackground/> */}
-
+    <div className="bg-custom-image bg-cover bg-center min-h-screen">        
       <Navbar />
-      {/* <div className="h-16"></div> Spacer div for navbar height */}
-      {/* <div className="bg-custom-image bg-cover bg-center min-h-screen"> */}
-        {/* <input
-          type="image"
-          src={img}
-          width="100px"
-          height="80px"
-          alt="Chatbot"
-          className="fixed bottom-4 right-4 cursor-pointer"
-          onClick={() => {
-            SerDirect(!direct);
-          }}
-        /> */}
-        {/* <input
-        type="image"
-        src={img}
-        width="100px"
-        height="80px"
-        alt=""
-        onClick={() => {
-          SerDirect(!direct);
-        }}
-      /> */}
-        {direct && <Bot />}
-      {/* </div> */}
-      {/* Other content can go here */}
     </div>
   );
 }
--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -39,7 +39,7 @@ function Navbar() {
         )}
   </div>
   <div class="hidden md:flex items-center justify-between w-full md:w-auto md:order-1 text-xl" id="navbar-user" >
-    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-20 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
+    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-20 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
       <li>
       <a href="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Profile</a>
       </li>
